bdf1949a89b7471e909ded583fa77f9c5068218e
Chethan-170 <chethanpoojary170@gmail.com>
2023-05-31 16:52:08 +0530

    usage of BlocProvider and BlocBuilder

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_tutorial/cubics/counter/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,21 +13,35 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text("Learning BloC"),
         ),
-        body: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Center(child: Text('The counter value is: '))]),
+        body: BlocBuilder<CounterCubit, CounterState>(
+          builder: (context, state) {
+            return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                      child: Text(
+                    'The counter value is : ${state.counter}',
+                    style: TextStyle(fontSize: 32),
+                  ))
+                ]);
+          },
+        ),
         floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             FloatingActionButton(
-              onPressed: () => {},
+              onPressed: () {
+                BlocProvider.of<CounterCubit>(context).decrement();
+              },
               child: const Icon(Icons.remove),
             ),
             const SizedBox(
               width: 10,
             ),
             FloatingActionButton(
-              onPressed: () => {},
+              onPressed: () {
+                BlocProvider.of<CounterCubit>(context).increment();
+              },
               child: const Icon(Icons.add),
             )
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:bloc_tutorial/app.dart';
+import 'package:bloc_tutorial/cubics/counter/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const App());
+  runApp(BlocProvider(
+    create: (context) => CounterCubit(),
+    child: App(),
+  ));
 }
