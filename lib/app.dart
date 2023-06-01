c688aa40bc39364db71ef7ae894daa2d7d2f82f0
Chethan-170 <chethanpoojary170@gmail.com>
2023-06-01 11:52:52 +0530

    bloc basics

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_tutorial/blocs/counter/counter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'other_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,27 +14,57 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text("Learning BloC"),
         ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                  child: Text(
-                'The counter value is :',
-                style: TextStyle(fontSize: 32),
-              ))
-            ]),
+        body: BlocListener<CounterBloc, CounterState>(
+          listener: (context, state) {
+            if (state.counter == 3) {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Text('counter is ${state.counter}'),
+                  );
+                },
+              );
+            } else if (state.counter == -1) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return OtherPage();
+                }),
+              );
+            }
+          },
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                    child: Text(
+                  'The counter value is : ${context.watch<CounterBloc>().state.counter}',
+                  style: TextStyle(
+                      fontSize:
+                          context.select((CounterBloc c) => c.state.counter > 0)
+                              ? 32
+                              : 16),
+                )),
+              ]),
+        ),
         floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                context.read<CounterBloc>().add(DecrementCounterEvent());
+              },
               child: const Icon(Icons.remove),
             ),
             const SizedBox(
               width: 10,
             ),
             FloatingActionButton(
-              onPressed: () {},
+              onPressed: () {
+                BlocProvider.of<CounterBloc>(context)
+                    .add(IncrementCounterEvent());
+              },
               child: const Icon(Icons.add),
             )
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:bloc_tutorial/app.dart';
+import 'package:bloc_tutorial/blocs/counter/counter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(
-    App(),
+    BlocProvider(
+      create: (context) => CounterBloc(),
+      child: App(),
+    ),
   );
 }
