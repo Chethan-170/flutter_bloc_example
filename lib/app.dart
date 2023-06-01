92953caec4e1e54fc0c46614369b3b817dabcc5e
Chethan-170 <chethanpoojary170@gmail.com>
2023-06-01 10:55:05 +0530

    initial set up

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,4 @@
-import 'package:bloc_tutorial/cubics/counter/counter_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'other_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,63 +9,29 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-              "Learning BloC - ${context.select((CounterCubit c) => c.state.counter)}"),
+          title: Text("Learning BloC"),
         ),
-        body: BlocConsumer<CounterCubit, CounterState>(
-          listener: (context, state) {
-            if (state.counter == 3) {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    content: Text('counter is ${state.counter}'),
-                  );
-                },
-              );
-            } else if (state.counter == -1) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) {
-                  return OtherPage();
-                }),
-              );
-            }
-          },
-          listenWhen: (previous, current) {
-            if (current.counter == 3) {
-              return false;
-            } else
-              return true;
-          },
-          builder: (context, state) {
-            return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Center(
-                      child: Text(
-                    'The counter value is :: ${state.counter}',
-                    style: TextStyle(fontSize: 32),
-                  ))
-                ]);
-          },
-        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                  child: Text(
+                'The counter value is :',
+                style: TextStyle(fontSize: 32),
+              ))
+            ]),
         floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             FloatingActionButton(
-              onPressed: () {
-                BlocProvider.of<CounterCubit>(context).decrement();
-              },
+              onPressed: () {},
               child: const Icon(Icons.remove),
             ),
             const SizedBox(
               width: 10,
             ),
             FloatingActionButton(
-              onPressed: () {
-                BlocProvider.of<CounterCubit>(context).increment();
-              },
+              onPressed: () {},
               child: const Icon(Icons.add),
             )
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:bloc_tutorial/app.dart';
-import 'package:bloc_tutorial/cubics/counter/counter_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(
-    BlocProvider(
-      create: (context) => CounterCubit(),
-      child: App(),
-    ),
+    App(),
   );
 }
