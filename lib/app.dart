1438bc8fdfc3147450e76c547fbed65e79c0f4d9
Chethan-170 <chethanpoojary170@gmail.com>
2023-06-04 13:46:57 +0530

    initial setup

--- lib/app.dart
+++ lib/app.dart
@@ -5,31 +5,45 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Learning BloC"),
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          '0',
+          style: const TextStyle(fontSize: 64.0),
         ),
-        body: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Center(child: Text('The counter value is: '))]),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () => {},
-              child: const Icon(Icons.remove),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            FloatingActionButton(
-              onPressed: () => {},
-              child: const Icon(Icons.add),
-            )
-          ],
-        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.brightness_6),
+          ),
+          const SizedBox(width: 5.0),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 5.0),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(width: 5.0),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.delete_forever),
+          ),
+        ],
       ),
     );
   }
