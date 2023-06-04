c6b612e9543228442176906451f9166650cf929e
Chethan-170 <chethanpoojary170@gmail.com>
2023-06-04 11:21:44 +0530

    transformers

--- lib/blocs/counter/counter_bloc.dart
+++ lib/blocs/counter/counter_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 
 part 'counter_event.dart';
@@ -6,13 +7,30 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterState.initial()) {
-    on<IncrementCounterEvent>((event, emit) {
-      emit(state.copyWith(counter: state.counter + 1));
-    });
-    on<DecrementCounterEvent>(_decrementCounter);
+    // on<IncrementCounterEvent>(
+    //   _handleIncrementEventCounter,
+    // );
+    // on<DecrementCounterEvent>(_handleDecrementEventCounter,
+    //     transformer: restartable());
+
+    on<CounterEvent>((event, emit) async {
+      if (event is IncrementCounterEvent) {
+        await _handleIncrementEventCounter(event, emit);
+      } else if (event is DecrementCounterEvent) {
+        await _handleDecrementEventCounter(event, emit);
+      }
+    }, transformer: sequential());
   }
 
-  void _decrementCounter(DecrementCounterEvent event, Emitter emitParam) {
-    emitParam(state.copyWith(counter: state.counter - 1));
+  Future<void> _handleIncrementEventCounter(
+      CounterEvent evt, Emitter<CounterState> emit) async {
+    await Future.delayed(Duration(seconds: 4));
+    emit(state.copyWith(counter: state.counter + 1));
+  }
+
+  Future<void> _handleDecrementEventCounter(
+      CounterEvent evt, Emitter<CounterState> emit) async {
+    await Future.delayed(Duration(seconds: 2));
+    emit(state.copyWith(counter: state.counter - 1));
   }
 }
