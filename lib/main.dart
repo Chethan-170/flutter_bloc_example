becdb82f290377a42221dcc837892dbea4888798
Chethan-170 <chethanpoojary170@gmail.com>
2023-06-04 10:52:03 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:bloc_tutorial/app.dart';
 import 'package:bloc_tutorial/blocs/color/color_bloc.dart';
 import 'package:bloc_tutorial/blocs/counter/counter_bloc.dart';
+import 'package:bloc_tutorial/observer/app_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Bloc.observer = AppBlocObserver();
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
